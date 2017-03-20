4e9d786b51664e2bd2a15d14e6e963954b899dda
Mikuláš Dítě <rullaf@gmail.com>
2017-03-20 09:16:35 +0100

    add no-dia corpora

---
+++ resources/languages/cs/corpus/numbers.no-dia.clj
@@ -0,0 +1,131 @@
+(
+  ; Context map
+  {}
+
+  "0"
+  "nic"
+  "nul"
+  "nula"
+  "zadna"
+  "zadni"
+  "zadne"
+  (number 0)
+
+  "1"
+  "jeden"
+  "jediny"
+  "jednu"
+  (number 1)
+
+  "2"
+  "dva"
+  (number 2)
+
+  "peti"
+  (number 5)
+
+  "33"
+  "tricet tri"
+  ;"triatricet"
+  (number 33)
+
+  "14"
+  "ctrnact"
+  (number 14)
+
+  "16"
+  "sestnact"
+  (number 16)
+
+  "17"
+  "sedmnact"
+  (number 17)
+
+  "18"
+  "osmnact"
+  (number 18)
+
+  "1.1"
+  "1.10"
+  "01.10"
+  "1,1"
+  "1,10"
+  "01,10"
+  "1 cela 1"
+  (number 1.1)
+
+  "0.77"
+  ".77"
+  "0,77"
+  ",77"
+  "zadna cela 77"
+  (number 0.77)
+
+  "zadna cela 101"
+  (number 0.101)
+
+  "100000"
+  ; "100 000"
+  "100K"
+  "100k"
+  (number 100000)
+
+  "3M"
+  "3000000"
+  ; "3 000 000"
+  (number 3000000)
+
+  ; "1 200 000"
+  "1200000"
+  "1.2M"
+  "1200K"
+  (number 1200000)
+
+  ; "-1 200 000"
+  "-1200000"
+  "-1.2M"
+  (number -1200000)
+
+  "5 tisic"
+  "pet tisic"
+  (number 5000)
+
+  "sto dvacet dva"
+  (number 122)
+
+  "dve ste dvacet dva"
+  (number 222)
+
+  "dve ste tisic"
+  (number 200000)
+
+  "dvacet jedna tisic jedenact"
+  (number 21011)
+
+  "sedm set dvacet jedna tisic dvanact"
+  (number 721012)
+
+  "jeden milion"
+  (number 1000000)
+
+  "sedesat pet milionu"
+  (number 65000000)
+
+  "tricet jedna milionu dve ste padesat sest tisic sedm set dvacet jedna"
+  (number 31256721)
+
+  "ctvrty"
+  (ordinal 4)
+
+  "patnacta"
+  "15-ty"
+  "15ty"
+  "15."
+  (ordinal 15)
+
+  "sedmdesaty"
+  (ordinal 70)
+
+  "osmdesata devata"
+  (ordinal 89)
+)
